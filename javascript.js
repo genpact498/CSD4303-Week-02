c1100f754b8cd2777e18416d2ec65847c4b28cb8
Subhajit Chakraborthy <738402@student.tor.loc>
2019-05-16 16:27:55 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let iteam1=20;
+let iteam2=50;
+let firstname="Subhajit";
+let lastname="Chakraborthy";
+let total=iteam1+iteam2;
+alert("Hello"+" "+firstname+" "+lastname+" Your total is "+total);
+                 
